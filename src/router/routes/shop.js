cf52cfe93a70697c26fe1e0e3e2c89b8768620ec
ping5214 <wangping521410@163.com>
2021-01-11 12:02:32 +0800

    createRoutes

---
+++ src/router/routes/shop.js
@@ -0,0 +1,24 @@
+import Shop from '@/views/Shop/Index'
+import Order from '@/views/Shop/Order'
+import Discount from '@/views/Shop/Discount'
+import Line from '@/views/Shop/Line'
+import Table from '@/views/Shop/Table'
+
+var shop = [
+    {
+        path: "/shop",
+        component: Shop,
+        children: [
+          // 点餐
+          {path: "order", component: Order},
+          // 优惠券
+          {path: "discount", component: Discount},
+          // 排队取号
+          {path: "line", component: Line},
+          // 餐桌预定
+          {path: "table", component: Table},
+        ]
+      },
+]
+
+export default shop
